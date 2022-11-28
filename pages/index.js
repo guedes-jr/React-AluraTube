67700022ce334c57186b4994f309765864c03692
João Guedes <joao.guedes.developer@gmail.com>
2022-11-28 00:37:15 -0300

    Atualizações do projeto

--- pages/index.js
+++ pages/index.js
@@ -1,55 +1,115 @@
-import config from "../config.json"
-import styled from "styled-components"
+import React from "react";
+import config from "../config.json";
+import styled from "styled-components";
+import Menu from "../src/components/Menu";
+import { StyledTimeline } from "../src/components/Timeline";
+import { videoService } from "../src/services/videoService";
 
 function HomePage() {
-  const estilosDaHome = { backgroundColor: 'red' }
+  const service = videoService();
+  const [valorDoFiltro, setValorDoFiltro] = React.useState("");
+  const [playlists, setPlaylists] = React.useState({});
+  React.useEffect(() => {
+    service
+      .getAllVideos()
+      .then((dados) => {
+        console.log(dados.data);
+        const novasPlaylists = { ...playlists };
+        dados.data.forEach((video) => {
+          if (!novasPlaylists[video.playlist]) {
+            novasPlaylists[video.playlist] = [];
+          };
+          novasPlaylists[video.playlist].push(video);
+        });
+        setPlaylists(novasPlaylists);
+      });
+  },
+    [],
+  );
   return (
-    <div style={estilosDaHome}>
-      <Menu />
-      <Header />
-      <TimeLine />
-    </div>
-  )
-}
+    <>
+      <div style={{
+        display: "flex",
+        flexDirection: "column",
+        flex: 1,
+      }}>
+        <Menu valorDoFiltro={valorDoFiltro} setValorDoFiltro={setValorDoFiltro} />
+        <Header />
+        <Timeline searchValue={valorDoFiltro} playlists={playlists}>Conteúdo</Timeline>
+      </div>
+    </>
+  );
+};
 
-export default HomePage
-
-function Menu() {
-  return (
-    <div>Menu</div>
-  )
-}
+export default HomePage;
 
 const StyledHeader = styled.div`
-  img {
-    width: 80px;
-    height: 80px;
-    border-radius: 50%;
-  }
+    background-color: ${({ theme }) => theme.backgroundLevel1};
+    img {
+        width: 80px;
+        height: 80px;
+        border-radius: 50%;
+    };
+    .user-info {
+        display: flex;
+        align-items: center;
+        width: 100%;
+        padding: 16px 32px;
+        gap: 16px;
+    };
+`;
 
-  .user-info {
-    display: flex;
-    align-items: center;
-    width: 100%;
-    padding: 16px 32px;
-    gap: 16px;
-  }
-`
+const StyledBanner = styled.div`
+    background-color: blue;
+    background-image: url(${({ bg }) => bg});
+    background-size: cover;
+    height: 230px;
+`;
+
 function Header() {
   return (
     <StyledHeader>
-      {/* <img src="banner" /> */}
+      <StyledBanner bg={config.banner} />
       <section className="user-info">
-        <img src={`http://github.com/${config.github}.png`} />
-        {config.name}
-        {config.job}
+        <img src={`https://github.com/${config.github}.png`} />
+        <div>
+          <h2>{config.name}</h2>
+          <p>{config.description}</p>
+        </div>
       </section>
     </StyledHeader>
-  )
-}
+  );
+};
 
-function TimeLine() {
+function Timeline({ searchValue, ...propriedades }) {
+  const playlistNames = Object.keys(propriedades.playlists);
   return (
-    <div>Time Line</div>
-  )
-}
+    <StyledTimeline>
+      {playlistNames.map((playlistName) => {
+        const videos = propriedades.playlists[playlistName];
+        return (
+          <section key={playlistName}>
+            <h2>{playlistName}</h2>
+            <div>
+              {videos
+                .filter((video) => {
+                  const titleNormalized = video.title.toLowerCase();
+                  const searchValueNormalized = searchValue.toLowerCase();
+                  return titleNormalized.includes(searchValueNormalized);
+                })
+                .map((video) => {
+                  return (
+                    <a key={video.url} href={video.url}>
+                      <img src={video.thumb} />
+                      <span>{video.title}</span>
+                    </a>
+                  );
+                })
+              }
+            </div>
+          </section>
+        );
+      })}
+    </StyledTimeline>
+  );
+};
